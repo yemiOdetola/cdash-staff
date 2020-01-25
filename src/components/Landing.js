f9919812a91f793d8aba8ee2b60f4d1d3477e013
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-01-25 10:01:45 +0100

    comps...land

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 
@@ -18,97 +19,40 @@ export class Landing extends Component {
                     <h3>All assets</h3>
                   </div>
                   <div className="assets">
-                    <div className="row">
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <span className="label">Fulltime</span>
-                          <h5>Front-end Designer</h5>
-                          <p>
-                            Squarespace Inc.
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <span className="label bg--primary-2">Remote</span>
-                          <h5>Art Director</h5>
-                          <p>
-                            Hobson &amp; Peterson
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <h5>Senior Interface Designer</h5>
-                          <p>
-                            Supply Co. (NYC)
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <h5>Design Internship</h5>
-                          <p>
-                            Instrument San Francisco
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <h5>Graphic Designer (Junior)</h5>
-                          <p>
-                            Paddle Financial
-                                </p>
-                          <a href="#">View &amp; Apply</a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <span className="label">Fulltime</span>
-                          <h5>Expression Engine Expert</h5>
-                          <p>
-                            Hoëffler
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <span className="label bg--primary-2">Remote</span>
-                          <h5>Full-stack Developer</h5>
-                          <p>
-                            Envato
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="feature feature-1 boxed boxed--border">
-                          <h5>Marketing Director</h5>
-                          <p>
-                            Clear Left
-                                </p>
-                          <a href="#">
-                            View &amp; Apply
-                                </a>
-                        </div>
-                      </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
+                    </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
+                    </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
+                    </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
+                    </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
+                    </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
+                    </div>
+                    <div className="each-asset">
+                      <div className="title">Summer walking</div>
+                      <div className="sub">Bust it down</div>
+                      <Link to={`/`}>Some Link</Link>
                     </div>
                   </div>
                 </div>
